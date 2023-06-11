dd0e104dc42916acaa473fd2fc1ff6b4c354a7a1
Marcin Chryczyk <marcin133mlynne@gmail.com>
2023-06-11 20:39:08 +0200

    Add test cases for pcd function
    This commit adds test cases for the pcd function. The test cases cover various scenarios, including navigating using shortcuts, navigating to an existing directory, navigating to a previous directory by index, navigating to a previous directory by path, handling invalid directory index, and handling invalid directory path.

--- tests/DirRecallTest.zsh
+++ tests/DirRecallTest.zsh
@@ -33,17 +33,57 @@ test_validate_opt() {
 # Test for _add_directory function
 test_add_directory() {
   local max_size=8
-  local prev_dirs=("/dir1" "/dir2" "/dir3" "/dir4")
+  local prev_dirs=("/dir1" "/dir2" "/dir3" "/dir4" "..." "..." "..." "...")
 
   # Test adding a directory to the array not fully array
   local result
   result=$(_add_directory "$max_size" "${prev_dirs[@]}")
-  assertEquals "Add directory" "$PWD /dir1 /dir2 /dir3 /dir4" "$result"
+  assertEquals "Add directory" "$PWD /dir1 /dir2 /dir3 /dir4 ... ... ..." "$result"
 
   # Test when the array is already at maximum size
   local max_dirs=("/dir1" "/dir2" "/dir3" "/dir4" "/dir5" "/dir6" "/dir7" "/dir8")
   result=$(_add_directory "$max_size" "${max_dirs[@]}")
   assertEquals "Maximum size reached" "$PWD /dir1 /dir2 /dir3 /dir4 /dir5 /dir6 /dir7" "$result"
+}
+
+# Test for pcd function
+test_pcd() {
+  # Test navigating using shortcuts
+  cd /var/tmp
+  cd /usr
+  cd /tmp
+  pcd tmp
+  assertEquals "Expected /var/tmp, but got $PWD" "/var/tmp" "$PWD"
+
+  # Test navigating to an existing directory
+  cd /tmp
+  pcd /tmp
+  assertEquals "Expected /tmp, but got $PWD" "/tmp" "$PWD"
+
+  # Test navigating to a previous directory by index
+  cd /tmp
+  cd /var
+  cd /usr
+  pcd 2
+  assertEquals "Expected /tmp, but got $PWD" "/tmp" "$PWD"
+
+  # Test navigating to a previous directory by path
+  cd /tmp
+  cd /var
+  cd /usr
+  pcd /var
+  assertEquals "Expected /var, but got $PWD" "/var" "$PWD"
+
+  # Test handling invalid directory index
+  cd /tmp
+  local target=$(( ${#_prev_dirs[@]} + 1 ))
+  pcd $target > /dev/null 2>&1 
+  assertEquals "Expected error message, but got exit code $?" 1 $?
+
+  # Test handlind invalid directory path
+  cd /tmp
+  pcd /invalid/path > /dev/null 2>&1 
+  assertEquals "Expected error message, but got exit code $?" 1 $?
 }
 
 # Purpose: Confirm functionality
